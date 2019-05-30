42f5e2be43a0a2f26f695305fc27460a45f12a90
Camila Luísa <camilalfl17@gmail.com>
2019-05-29 22:28:30 -0300

    Code Update

--- isolation_forest/isolationforest.py
+++ isolation_forest/isolationforest.py
@@ -1,28 +1,28 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import csv
 from sklearn.ensemble import IsolationForest
 
 ### READ FILE.CSV
-# sample1 = pd.read_csv('teste1.csv',usecols=['NR_VOTAVEL','RECEITA_TOTAL','QTD_VOTOS','CUSTO_VOTO','VR_TOTAL_BEM_CANDIDATO','Recursos_partido','Recursos_proprios','Recursos_outros_candidatos','Recursos_pessoas_fisicas','Rendimentos_aplicacoes','Financiamento_Coletivo','origens_nao_identificadas'], sep=',', low_memory=False, encoding='latin-1')
-sample1 = pd.read_csv('deputados.csv',usecols=['NR_VOTAVEL','CARGO_N','SITUCAO_N','RECEITA_TOTAL','QTD_VOTOS','CUSTO_VOTO','VR_TOTAL_BEM_CANDIDATO','Recursos_partido','Recursos_proprios','Recursos_outros_candidatos','Recursos_pessoas_fisicas','Rendimentos_aplicacoes','Financiamento_Coletivo','origens_nao_identificadas','Comercio_bens_ou_eventos','Doacoes_Internet','DESPESA_CONTRATADA'], sep=';', low_memory=False, encoding='latin-1').fillna(value = 0)
+sample1 = pd.read_csv('deputados.csv',usecols=['SQ_CANDIDATO','NR_VOTAVEL','CARGO_N','SITUCAO_N','RECEITA_TOTAL','QTD_VOTOS','CUSTO_VOTO','VR_TOTAL_BEM_CANDIDATO','Recursos_partido','Recursos_proprios','Recursos_outros_candidatos','Recursos_pessoas_fisicas','Rendimentos_aplicacoes','Financiamento_Coletivo','origens_nao_identificadas','Comercio_bens_ou_eventos','Doacoes_Internet','DESPESA_CONTRATADA'], sep=';', low_memory=False, encoding='latin-1').fillna(value = 0)
 
 sample = sample1.values
 print(sample.shape)
 #Isolation Forest
 ##
-clf = IsolationForest(max_samples='auto', contamination=0.10, n_jobs=-1, behaviour="new") 
+clf = IsolationForest(max_samples='auto', contamination=0.15, n_jobs=-1, behaviour="new") 
  #contamination pega uma porcentagem da base
-#n_jobs define quantos nucleos de hardware serao usados
+#n_jobs define quantos nucleos de hardware serão usados
 
 clf.fit(sample)#ajustando o modelo do isolation forest para a base
 
 scores = clf.decision_function(sample)#classifica os candidatos de -1 a 1 onde quanto mais proximo de -1 mais anomalo
 
-predict  = clf.predict(sample) #classifica os candidatos so entre -1 e 1 mostrar em um grafico
-#testar varios valores de contamination, quando comecar a pegar candidatos que nao sao anomalias ja pode parar
+predict  = clf.predict(sample) #classifica os candidatos só entre -1 e 1 mostrar em um gráfico
+#testar varios valores de contamination, quando começar a pegar candidatos que não são anomalias já pode parar
 
-prop = (float(predict.tolist().count(-1)) / float(len(sample))) #proporcao de outliers/base pode tirar
+prop = (float(predict.tolist().count(-1)) / float(len(sample))) #propo~ção de outliers/base pode tirar
 
 num_outliers = predict.tolist().count(-1) #numero de outliers
 
